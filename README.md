e50fba9505fcb050b03eb8600d4f9fec1b31d95f
DarkHorse1997 <tanmoydas1997@gmail.com>
2020-09-17 23:57:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-b89vua
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-b89vua)
---
+++ src/data.js
@@ -0,0 +1,251 @@
+
+const data = [
+  {
+    id: 1,
+    title: "Lorem",
+    // image: image1,
+    list: [
+      {
+        id: 1,
+        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+      },
+      {
+        id: 2,
+        text: "Aliquam tincidunt mauris eu risus."
+      },
+      {
+        id: 3,
+        text: "Vestibulum auctor dapibus neque."
+      },
+      {
+        id: 4,
+        text: "Nunc dignissim risus id metus."
+      },
+      {
+        id: 5,
+        text: "Cras ornare tristique elit."
+      }
+    ]
+  },
+  {
+    id: 2,
+    // image: image2,
+    title: "Ipsum",
+    list: [
+      {
+        id: 1,
+        text: "Vivamus vestibulum ntulla nec ante."
+      },
+      {
+        id: 2,
+        text: "Praesent placerat risus quis eros."
+      },
+      {
+        id: 3,
+        text: "Fusce pellentesque suscipit nibh."
+      },
+      {
+        id: 4,
+        text: "Integer vitae libero ac risus egestas placerat."
+      },
+      {
+        id: 5,
+        text: "Vestibulum commodo felis quis tortor."
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Dolor",
+    // image: image3,
+    list: [
+      {
+        id: 1,
+        text: "Ut aliquam sollicitudin leo."
+      },
+      {
+        id: 2,
+        text: "Cras iaculis ultricies nulla."
+      },
+      {
+        id: 3,
+        text: "Donec quis dui at dolor tempor interdum."
+      },
+      {
+        id: 4,
+        text: "Phasellus ultrices nulla quis nibh. "
+      },
+      {
+        id: 5,
+        text:
+          "Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus."
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "Sit",
+    // image: image4,
+    list: [
+      {
+        id: 1,
+        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+      },
+      {
+        id: 2,
+        text: "Aliquam tincidunt mauris eu risus."
+      },
+      {
+        id: 3,
+        text: "Vestibulum auctor dapibus neque."
+      },
+      {
+        id: 4,
+        text: "Nunc dignissim risus id metus."
+      },
+      {
+        id: 5,
+        text: "Cras ornare tristique elit."
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: "Amet",
+    // image: image5,
+    list: [
+      {
+        id: 1,
+        text: "Vivamus vestibulum ntulla nec ante."
+      },
+      {
+        id: 2,
+        text: "Praesent placerat risus quis eros."
+      },
+      {
+        id: 3,
+        text: "Fusce pellentesque suscipit nibh."
+      },
+      {
+        id: 4,
+        text: "Integer vitae libero ac risus egestas placerat."
+      },
+      {
+        id: 5,
+        text: "Vestibulum commodo felis quis tortor."
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: "Consectetuer",
+    // image: image6,
+    list: [
+      {
+        id: 1,
+        text: "Ut aliquam sollicitudin leo."
+      },
+      {
+        id: 2,
+        text: "Cras iaculis ultricies nulla."
+      },
+      {
+        id: 3,
+        text: "Donec quis dui at dolor tempor interdum."
+      },
+      {
+        id: 4,
+        text: "Phasellus ultrices nulla quis nibh. "
+      },
+      {
+        id: 5,
+        text:
+          "Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus."
+      }
+    ]
+  },
+  {
+    id: 7,
+    title: "Adipiscing",
+    // image: image7,
+    list: [
+      {
+        id: 1,
+        text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+      },
+      {
+        id: 2,
+        text: "Aliquam tincidunt mauris eu risus."
+      },
+      {
+        id: 3,
+        text: "Vestibulum auctor dapibus neque."
+      },
+      {
+        id: 4,
+        text: "Nunc dignissim risus id metus."
+      },
+      {
+        id: 5,
+        text: "Cras ornare tristique elit."
+      }
+    ]
+  },
+  {
+    id: 8,
+    title: "Elit",
+    // image: image8,
+    list: [
+      {
+        id: 1,
+        text: "Vivamus vestibulum ntulla nec ante."
+      },
+      {
+        id: 2,
+        text: "Praesent placerat risus quis eros."
+      },
+      {
+        id: 3,
+        text: "Fusce pellentesque suscipit nibh."
+      },
+      {
+        id: 4,
+        text: "Integer vitae libero ac risus egestas placerat."
+      },
+      {
+        id: 5,
+        text: "Vestibulum commodo felis quis tortor."
+      }
+    ]
+  },
+  {
+    id: 9,
+    title: "Donec",
+    // image: image9,
+    list: [
+      {
+        id: 1,
+        text: "Ut aliquam sollicitudin leo."
+      },
+      {
+        id: 2,
+        text: "Cras iaculis ultricies nulla."
+      },
+      {
+        id: 3,
+        text: "Donec quis dui at dolor tempor interdum."
+      },
+      {
+        id: 4,
+        text: "Phasellus ultrices nulla quis nibh. "
+      },
+      {
+        id: 5,
+        text:
+          "Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus."
+      }
+    ]
+  }
+];
+
+export default data;
