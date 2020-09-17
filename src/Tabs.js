29eee629933a070b57cb83df900be594b5628ea9
Tanmoy Das <tanmoydas1997@gmail.com>
2020-09-18 01:58:45 +0530

    first ver

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,35 +1,99 @@
-import React from "react";
+import React,{useState} from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import Card from './card'
+import data from './data'
 
 const StyledMain = styled(motion.main)`
   height: 80vh;
   width: 80vw;
   display: grid;
-  background: blue;
+  // background: blue;
   grid-template-areas:
     "tabs"
     "tabcontent";
+  grid-template-rows: 3fr 8fr;
 `;
 
 const StyledGrid = styled(motion.div)`
   grid-area: "tabs";
-  display: grid;
-  background: red;
+  display: flex;
+  justify-content:space-evenly;
+  align-items:center;
+  // background: red;
+  background:whitesmoke;
 `;
 
 const StyledContent = styled(motion.div)`
   grid-area: "tabcontent";
   display: flex;
   flex-direction: column;
-  background: green;
+  align-items:center;
+  justify-content:center;
+  background: whitesmoke;
+  overflow:auto;
+  padding:10px;
 `;
 
+const StyledList = styled(motion.ul)`
+  list-style-type:disc;
+`
+const StyledListItem = styled(motion.li)`
+
+`
+const StyledImage = styled(motion.img)`
+  display:block;
+  padding:10px;
+  margin-top:20px;
+  height:calc(400px *0.8);
+  width:calc(500px * 0.8);
+  
+`
+
+const Content = ({image, list, title}) => {
+
+  return(
+    <>
+    <StyledContent>
+
+
+    {image && 
+      <StyledImage 
+         src={image} 
+         alt={title}
+         initial={{x:-100}}
+         animate={{x:0}}
+         />}
+
+    {list && <StyledList>{
+        list.map(listitem => <StyledListItem key={listitem.id}>{listitem.text}</StyledListItem>)
+    }
+      </StyledList>}
+      </StyledContent>
+      </>
+      
+  )
+};
+
 const Main = (props) => {
+  const [active, setActive] = useState(0);
+  const [details, setDetails] =  useState({});
+  const deets = data.find(card=> card.id === active)
+  // console.log(deets.image)
+
   return (
     <StyledMain>
-      <StyledGrid />
-      <StyledContent />
+      <StyledGrid>
+        {data.map((tab) => (
+          <Card key={tab.id} 
+                id={tab.id} 
+                title={tab.title} 
+                active={active}
+                setActive={setActive}
+                />
+        ))}
+      </StyledGrid>
+      {deets && <Content image={deets.image} title={deets.title} list={deets.list}/>}
     </StyledMain>
   );
 };
--- src/card.js
+++ src/card.js
@@ -3,9 +3,41 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const StyledCard = styled(motion.div)`
-  color: yellow;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-color:green;
+  height:150px;
+  width:100px;
+  border:2px solid white;
+
+  &:hover{
+      color:grey;
+      cursor:pointer;
+  }
+
+  &.active{
+      background-color:whitesmoke;
+      color:black;
+      border:2px solid black;
+  }
 `;
 
 const Card = (props) => {
-  return <StyledCard></StyledCard>;
+
+    const handleClick = () => {
+        props.setActive(props.id)
+    }
+
+  return( <StyledCard 
+            onClick={handleClick}
+            className = {props.id === props.active ? "active": ""}
+                    >
+      {props.title}
+      </StyledCard>
+    )
+  ;
 };
+
+export default Card;
